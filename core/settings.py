c134b3ace91b1073a0a9de61d2eb9dd1d2f8ef82
Judson Bomfim <falecom@judsonbomfim.com>
2025-07-15 19:36:48 -0300

    Correção

--- core/settings.py
+++ core/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_celery_beat',
     'rest_framework',
     'sass_processor',
     'widget_tweaks',
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 python manage.py migrate
 python manage.py collectstatic --noinput
-nohup celery -A core worker -B --loglevel=info
+celery -A core worker --loglevel=info &
+gunicorn core.wsgi:application --bind 0.0.0.0:8000
 exec "$@"
