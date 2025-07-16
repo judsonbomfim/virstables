71734774bbb0e310eb7fa543681b6d2bfc83fe6e
Judson Bomfim <falecom@judsonbomfim.com>
2025-07-16 12:49:25 -0300

    alteração do celey beat

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-
 python manage.py migrate
 python manage.py collectstatic --noinput
 nohup gunicorn core.wsgi:application --bind 0.0.0.0:8000 --log-level=info &
-nohup celery -A core worker -B --loglevel=info &
+nohup celery -A core worker --loglevel=info &
