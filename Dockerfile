b156c0dc5cb7a36230639a820336b3e87305ef37
Judson Bomfim <falecom@judsonbomfim.com>
2025-07-16 10:32:09 -0300

    Implementação Default

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,22 @@
-# Estágio de build
-FROM python:3.12-slim AS builder
-WORKDIR /app
-COPY requirements.txt .
-RUN pip install --no-cache-dir -r requirements.txt
-
-# Estágio final
-FROM python:3.12-slim
-WORKDIR /app
-RUN apt-get update && apt-get install -y netcat-openbsd && rm -rf /var/lib/apt/lists/*
-COPY --from=builder /usr/local/lib/python3.12/site-packages/ /usr/local/lib/python3.12/site-packages/
-COPY . .
+FROM python:3.12.1-slim-bullseye
+ENV PYTHONDONTWRITEBYTECODE=1
 ENV PYTHONUNBUFFERED=1
+WORKDIR /djangoweb
+
+COPY requirements.txt .
+
+RUN apt-get update && apt-get install -y nano && \
+    pip install --upgrade pip && \
+    pip install -r requirements.txt && \
+    adduser --disabled-password --no-create-home duser
+
+COPY . .
+
+COPY entrypoint.sh ./scripts/entrypoint.sh
+
+RUN chmod +x /djangoweb/scripts/entrypoint.sh && \
+    chown -R duser:duser /djangoweb
+
+USER duser
+
 EXPOSE 8000
-ENTRYPOINT ["./entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,29 +1,6 @@
 #!/bin/sh
 
-# Aguarda a conexão com o banco de dados (local)
-until nc -z 127.0.0.1 5432; do
-    echo "Aguardando PostgreSQL..."
-    sleep 2
-done
-
-# Aguarda a conexão com o Redis (local)
-until nc -z 127.0.0.1 6379; do
-    echo "Aguardando Redis..."
-    sleep 2
-done
-
-# Executa migrações e coleta arquivos estáticos (apenas no serviço web)
-if [ "$1" = "web" ]; then
-    # python manage.py migrate --noinput
-    python manage.py collectstatic --noinput
-fi
-
-# Inicia o Gunicorn ou Celery conforme o serviço
-if [ "$1" = "web" ]; then
-    exec gunicorn --bind 0.0.0.0:8000 core.wsgi:application --timeout 120
-elif [ "$1" = "celery_worker" ]; then
-    exec celery -A core worker -l info
-else
-    echo "Serviço não reconhecido. Use 'web' ou 'celery_worker'."
-    exit 1
-fi
+python manage.py migrate
+python manage.py collectstatic --noinput
+nohup gunicorn core.wsgi:application --bind 0.0.0.0:8000 --log-level=info &
+nohup celery -A core worker -B --loglevel=info
