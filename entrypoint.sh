700e5b6ff50b2b36b916ce01f43b46deddfc5d5f
Judson Bomfim <falecom@judsonbomfim.com>
2025-07-31 09:15:45 -0300

    Ajustes

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,4 @@
 python manage.py migrate
 python manage.py collectstatic --noinput
 nohup gunicorn core.wsgi:application --bind 0.0.0.0:8000 --log-level=info --timeout 120 
-# nohup celery -A core worker --loglevel=info &:
 tail -f /dev/null
