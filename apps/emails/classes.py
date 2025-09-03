a564beaca70b91bafe042946792d01f3bd91750b
Judson Bomfim <falecom@judsonbomfim.com>
2025-09-03 17:54:27 -0300

    Debug email

--- apps/emails/classes.py
+++ apps/emails/classes.py
@@ -7,6 +7,14 @@ from django.core.mail import EmailMultiAlternatives
 
 def send_email(html_content, text_content, subject, email_cliente):
 
+    print('>>>>>>>>>>>>>>>>>>>> Enviando e-mail para:', email_cliente)
+    print('>>>>>>>>>>>>>>>>>>>> Assunto:', subject)
+    print('>>>>>>>>>>>>>>>>>>>> Corpo do e-mail:')
+    print(html_content)
+    print('>>>>>>>>>>>>>>>>>>>> Corpo do e-mail em texto:')
+    print(text_content)
+    print('>>>>>>>>>>>>>>>>>>>>')
+
     email = EmailMultiAlternatives(
         #subject
         subject,
@@ -15,7 +23,7 @@ def send_email(html_content, text_content, subject, email_cliente):
         #from email
         f"{settings.PAINEL_TITLE} <{settings.DEFAULT_FROM_EMAIL}>",
         #to
-        email_cliente,
+        [email_cliente] if isinstance(email_cliente, str) else email_cliente,
     )
     email.attach_alternative(html_content, "text/html")
     email.send()
