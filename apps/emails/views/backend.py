5a2df9bfda847fc0f393c121d9214bafc0af65d8
Judson Bomfim <falecom@judsonbomfim.com>
2025-09-03 15:25:51 -0300

    Correções

--- apps/emails/views/backend.py
+++ apps/emails/views/backend.py
@@ -4,9 +4,9 @@ from django.shortcuts import render
 from apps.emails.tasks import email_lance_confirmado
 
 
-def viewEmail(request, id):
+def viewEmail(request, email_id):
     
-    email_lance_confirmado(id)
+    email_lance_confirmado.delay(email_id)
     
     context = {
         'url_site': 'https://www.virtualstables.com.br/',
