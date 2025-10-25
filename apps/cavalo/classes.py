86c114d7e04c2f58cc9289b1f98b064ec1de2ce1
Judson Bomfim <falecom@judsonbomfim.com>
2025-10-24 23:55:16 -0300

    Correções

--- apps/cavalo/classes.py
+++ apps/cavalo/classes.py
@@ -4,7 +4,13 @@ from apps.leilao.models import Leilao
 def exibirLance(leilao):
     hoje_date = datetime.date.today()
     leilao = Leilao.objects.get(id=leilao)
-    exibir_lance = 'n'   
-    if leilao.data_inicio <= hoje_date <= leilao.data_fim:
-        exibir_lance = 's'    
+    exibir_lance = 'n'
+    
+    # Converter as datas do leilão para date
+    data_inicio = leilao.data_inicio.date() if hasattr(leilao.data_inicio, 'date') else leilao.data_inicio
+    data_fim = leilao.data_fim.date() if hasattr(leilao.data_fim, 'date') else leilao.data_fim
+    
+    if data_inicio <= hoje_date <= data_fim:
+        exibir_lance = 's'
+        
     return exibir_lance
